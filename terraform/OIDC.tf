73910e68b8f0393fafe32572feeb7fdf76f75cf7
Egbogu Chinedu Joseph <137600196+essiewakukha@users.noreply.github.com>
2026-09-01 21:12:44 +0300

    adding screenshot proof and OIDC

---
+++ terraform/OIDC.tf
@@ -0,0 +1,89 @@
+# Lets GitHub Actions assume an AWS role using short-lived tokens instead
+# of long-lived access keys stored as secrets. GitHub proves its identity
+# via a signed OIDC token; AWS trusts that token only for this specific repo.
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+variable "ghttps://github.com/essiewakukha/task-notes-apio" {
+  description = "GitHub repo allowed to assume the deploy role, as owner/repo"
+  type        = string
+
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repo}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name               = "${var.project_name}-github-actions-deploy"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_actions_deploy_permissions" {
+  statement {
+    sid    = "CoreServices"
+    effect = "Allow"
+    actions = [
+      "lambda:*",
+      "apigateway:*",
+      "dynamodb:*",
+      "sns:*",
+      "sqs:*",
+      "logs:*",
+      "cloudwatch:*",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "IAMForTerraformManagedRoles"
+    effect = "Allow"
+    actions = [
+      "iam:GetRole",
+      "iam:CreateRole",
+      "iam:DeleteRole",
+      "iam:PutRolePolicy",
+      "iam:GetRolePolicy",
+      "iam:DeleteRolePolicy",
+      "iam:PassRole",
+      "iam:TagRole",
+      "iam:ListRolePolicies",
+      "iam:ListAttachedRolePolicies",
+    ]
+    resources = ["arn:aws:iam::*:role/${var.project_name}-*"]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy_permissions" {
+  name   = "${var.project_name}-github-actions-deploy-permissions"
+  role   = aws_iam_role.github_actions_deploy.id
+  policy = data.aws_iam_policy_document.github_actions_deploy_permissions.json
+}
+
+output "github_actions_deploy_role_arn" {
+  description = "Put this in the AWS_DEPLOY_ROLE_ARN GitHub Actions secret"
+  value       = aws_iam_role.github_actions_deploy.arn
+}
